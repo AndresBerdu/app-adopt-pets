174a84e10e04b2853b8a22fb4f0cd94a5c40a6b4
AndresBerdu <andres700crac@gmail.com>
2024-02-01 19:58:46 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Sidebar } from './components/Sidebar';
+import "../src/sass/App.scss";
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
